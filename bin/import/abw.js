3ea72554cba23b0aa7b6a3c93e21832591c08383
Andy Wardley <abw@wardley.org>
2023-06-17 09:40:27 +0100

    Fixed up some color import issues

--- bin/import/abw.js
+++ bin/import/abw.js
@@ -1,11 +1,11 @@
 #!/usr/bin/env node
 import { bin } from '@abw/badger-filesystem'
-import { approximateCurvesFromStops } from '../../lib/utils/color.js'
+import { addHexToStops, approximateCurvesFromStops, interpolate5Stops } from '../../lib/utils/color.js'
 
-const infile  = 'config/source/abw-2.json'
-const outfile = 'config/palette/abw-2.json'
-const name    = 'abw-2'
-const comment = 'Palette #2 created by Andy Wardley'
+const infile  = 'config/source/abw.json'
+const outfile = 'config/palette/abw.json'
+const name    = 'abw'
+const comment = 'Palette created by Andy Wardley'
 const source  = ''
 const root    = bin().up(2)
 const colors  = await root.file(infile, { codec: 'auto' }).read()
@@ -14,6 +14,8 @@ const ranges = colors.ranges
 Object.entries(ranges).reduce(
   (ranges, [ , range]) => {
     range.curves = approximateCurvesFromStops(range.stops)
+    interpolate5Stops(range.stops)
+    addHexToStops(range.stops)
     return ranges
   },
   { }
