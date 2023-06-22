4c29f97edfc394b7333e6c04387f099700460aa1
Andy Wardley <abw@wardley.org>
2023-06-22 17:13:59 +0100

    Export library

---
+++ bin/export/badger.js
@@ -0,0 +1,25 @@
+#!/usr/bin/env node
+import { bin } from '@abw/badger-filesystem'
+import { exportPaletteCSSDir, exportPaletteSCSSDir } from '../../lib/export/palette.js'
+
+const infile  = 'config/palette/badger.json'
+const outdir  = 'export/badger'
+const root    = bin().up(2)
+
+const palette = await root
+  .file(infile, { codec: 'auto' })
+  .read()
+
+const scssdir = root.dir(`${outdir}/scss`)
+await scssdir.mustExist({ create: true })
+
+const cssdir = root.dir(`${outdir}/css`)
+await cssdir.mustExist({ create: true })
+
+// console.log(`palette: `, palette)
+console.log(`Exporting SCSS to ${scssdir}`)
+await exportPaletteSCSSDir(palette, scssdir)
+
+console.log(`Exporting CSS to ${cssdir}`)
+await exportPaletteCSSDir(palette, cssdir)
+
