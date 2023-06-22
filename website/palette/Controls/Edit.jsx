acf0f485902d6f7d3b1119b8b03a6df416666885
Andy Wardley <abw@wardley.org>
2023-06-22 13:11:57 +0100

    Palette edit

--- website/palette/Controls/Edit.jsx
+++ website/palette/Controls/Edit.jsx
@@ -1,13 +1,120 @@
-import React from 'react'
-import Link from '../../site/Link.jsx'
+import React, { useState } from 'react'
+import Button from '../../ui/Button.jsx'
+import Modal from '../../ui/Modal.jsx'
+import { URLS } from '../../site/URLS.jsx'
+import { Consumer } from '../../palettes/Context.jsx'
+import { useNavigate } from 'react-router-dom'
+import { Form, Field, Fields, useForm } from '@abw/react-formula'
+import * as yup  from 'yup'
+import { nameToURI } from '../../../lib/utils/index.js'
 
-const Edit = () =>
-  <Link
-    to="edit"
-    color="blue"
-    icon="pen"
-    solid button
-    className="mar-l-2"
+const fields = {
+  name: {
+    label: 'Name',
+    required: true,
+    help: 'A name for the palette'
+  },
+  uri: {
+    label: 'URI',
+    required: true,
+    help: 'A unique identifier used for links',
+    prepareValue: value => nameToURI(value),
+    validateOnChange: true,
+    minValidateLength: 1,
+  },
+  comment: {
+    label: 'Comment',
+    help: 'An optional comment about this palette'
+  },
+  source: {
+    label: 'Source',
+    help: 'An optional URL indicating the source of the palette',
+    validate: value =>
+      value.length
+        ? yup
+          .string()
+          .trim()
+          .matches(/^https?:\/\//, 'The source should be a URL starting with http:// or https://')
+          .validate(value)
+        : value
+  }
+}
+
+const Edit = ({ palette, palettes, editPalette }) => {
+  const navigate = useNavigate()
+  const [open, setOpen] = useState(false)
+  const [form, setForm] = useState(false)
+  const initialURI = palette.uri
+  const cancel = () => {
+    setOpen(false)
+  }
+  const submit = values => {
+    editPalette(values)
+    setOpen(false)
+    navigate(URLS.palette.home(values.uri))
+  }
+  const checkURIAvailable = (value, field, resolve, reject) => {
+    const existing = palettes[value]
+    if (existing && value !== initialURI) {
+      reject({
+        value,
+        message: `${value} is already being used`
+      })
+    }
+    resolve({
+      value,
+      message: `URI is available`
+    })
+  }
+
+  return (
+    <>
+      <Button
+        color="blue"
+        icon="pen"
+        onClick={() => setOpen(true)}
+        className="mar-l-2"
+        solid
+      />
+      <Modal open={open}>
+        <Form
+          values={palette} fields={fields}
+          onLoad={setForm}
+          onSubmit={submit}
+        >
+          <NameField/>
+          <Field
+            name="uri"
+            validate={checkURIAvailable}
+          />
+          <Fields names="comment source"/>
+        </Form>
+        <div className="flex space mar-t-8">
+          <Button
+            color="grey"
+            text="Cancel"
+            iconRight="cross"
+            onClick={cancel}
+          />
+          <Button
+            color="green"
+            text="Save"
+            iconRight="check"
+            solid
+            onClick={e => form.submit(e)}
+          />
+        </div>
+      </Modal>
+    </>
+  )
+}
+
+const NameField = () => {
+  const { setValues } = useForm()
+  return <Field
+    name="name"
+    onChange={field => setValues({ uri: nameToURI(field.value) })}
   />
+}
 
-export default Edit
+export default Consumer(Edit)
--- website/palette/Header.jsx
+++ website/palette/Header.jsx
@@ -1,14 +1,12 @@
 import React from 'react'
 import Controls from './Controls/index.jsx'
-import Name from './Name.jsx'
 import { Consumer } from '../palettes/Context.jsx'
-// import Options from '../palettes/Options.jsx'
 
 const Header = ({ palette }) =>
   <header>
     <div className="flex space start">
       <div>
-        <Name/>
+        <h1>{palette.name}</h1>
         <div className="info">
           { palette.comment &&
             <div className="comment">
@@ -24,7 +22,6 @@ const Header = ({ palette }) =>
       </div>
       <Controls/>
     </div>
-    {/* <Options/> */}
   </header>
 
 export default Consumer(Header)
